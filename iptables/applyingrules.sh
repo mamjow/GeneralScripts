6e3491fff49de60131dc259c78255bc66d090c21
M.J. Moshiri <mamjo@scenius.nl>
2023-01-14 23:18:16 +0100

    echo seems to do it cleaner

--- iptables/applyingrules.sh
+++ iptables/applyingrules.sh
@@ -3,8 +3,8 @@ function myfunc() {
     total=$1
     nr=$2
     value=$(((($nr + 1) * 100) / $total))
-    printf '\033[1A\033[K'
-    printf "%s%% IP: %s" $value $3
+    # printf "\r%s%% IP: %s" $value $3
+    echo -e "\e[1A\e[K${value}%  IP: ${3}"
 }
 scriptPath=$(dirname $0)
 whitelistInput="${scriptPath}/cidrIp.txt"
@@ -16,7 +16,7 @@ for ((i = 0 ; i < $wLineNr ; i++)); do
     iprange=$( sed -n "${i}p" < $whitelistInput )
     # echo $iprange
     # echo  iptables -A INPUT -s $iprange -j ACCEPT 
-    iptables -A INPUT -s $iprange -j ACCEPT
+    # iptables -A INPUT -s $iprange -j ACCEPT
     myfunc $wLineNr $i $iprange
 done
 
