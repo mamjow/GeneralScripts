ec878ef16ed04d5d274f97a53a1d440cd27b92a5
M.J. Moshiri <mamjo@scenius.nl>
2023-01-14 21:58:16 +0100

    improvements

---
+++ iptables/applyingrules.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+$1
+function myfunc() {
+    total=$1
+    nr=$2
+    value=$(((($nr + 1) * 100) / $total))
+    # echo -e "\033[5A"
+    printf "\r%s%%" $value
+}
+
+whitelistInput="${1}/iptables/cidrIp.txt"
+wlineNr=$(wc -l < $whitelistInput)
+printf "Total of %s rules to apply..." $wlineNr
+
+for ((i = 0 ; i < $wlineNr ; i++)); do
+    #  do iptable command
+    iprange=$(sed -n "${i}p" < $whitelistInput)
+    sudo iptables -A INPUT -s $iprange -j ACCEPT 
+    myfunc $wlineNr $i
+done
+
+printf "\n"
+printf "time to ban whole word"
+sudo iptables -P INPUT DROP
+printf "\n"
+printf "Enjoy"
+printf "\n"
+
