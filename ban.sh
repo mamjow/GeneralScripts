055a6fbb124cf1110efa40dd4b8f2cd2c81de376
M.J. Moshiri <mamjo@scenius.nl>
2023-01-15 13:26:21 +0100

    nvm for now

--- ban.sh
+++ ban.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-start=`date +%s`
 scriptPath=$(dirname $0)
 sudo chmod +x "${scriptPath}/iptables/applyingrules.sh"
 echo "Hoi hoi"
@@ -10,9 +9,6 @@ while true; do
             echo "Applying ACCEPT rules";
             # ( exec "./iptables/applyingrules.sh" )
             bash "${scriptPath}/iptables/applyingrules.sh"
-            end=`date +%s.%N
-            runtime=$( echo "$end - $start" | bc -l )
-            echo "applying all rules took $runtime";
             exit
         break;;
         [Nn]* ) exit;;
